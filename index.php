37939c1e121a060fed2c141beba4884d29a447d1
hanandestiarin <hananksndrian@gmail.com>
2022-07-18 10:18:14 +0700

    update

---
+++ index.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="author" content="templatemo">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <title>website Ekstrakulikuler SMA Xaverius Baturaja</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-snapx-photography.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+<!--
+
+TemplateMo 576 SnapX Photography
+
+https://templatemo.com/tm-576-snapx-photography
+
+-->
+  </head>
+
+<body>
+
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="home.php" class="logo">
+                        <img src="assets/images/logo.png" alt="SnapX Photography Template">
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li><a href="index.html" class="active"></a></li>
+                        
+                        <li><a href=""></a></li>
+                        <li><a href=""></a></li>
+                    </ul>   
+                    <div class="border-button">
+                      <a  href="login.php" class="sign-in-up"><i class="fa fa-user"></i> Login/Register</a>
+                    </div>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div id="modal" class="popupContainer" style="display:none;">
+    <div class="popupHeader">
+        <span class="header_title">Login</span>
+        <span class="modal_close"><i class="fa fa-times"></i></span>
+    </div>
+
+    <section class="popupBody">
+        <!-- Social Login -->
+        <div class="social_login">
+            
+
+            <div class="centeredText">
+                <span>Selamat Datang!</span>
+            </div>
+
+            <div class="action_btns">
+                <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
+                <div class="one_half last"><a href="#" id="register_form" class="btn">Register</a></div>
+            </div>
+        </div>
+
+        <!-- Username & Password Login form -->
+        
+
+            
+        </div>
+
+       
+        
+    </section>
+  </div>
+
+  <!-- ***** Main Banner Area Start ***** -->
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 offset-lg-1">
+          <div class="header-text">
+            <h2>Website Ekstrakulikuler <em>Olahraga</em>, <em>Seni </em> &amp; <em>Organisasi </em><br>SMA Xaverius Baturaja</br></h2>
+            <p>Website Ekstrakulikuler SMA Xaverius Baturaja adalah Website yang digunakan oleh para siswa untuk mendaftar Ekstrakulikuler olahraga, seni & Organisasi secara mudah dan dapat dilakukan dimana saja.</p>
+            <div class="buttons">
+              <div class="big-border-button">
+                <a href="login.php">Daftar Sekarang !!</a>
+              </div>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Main Banner Area End ***** -->
+
+
+  <section class="featured-items" id="featured-items">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="owl-features owl-carousel" style="position: relative; z-index: 5;">
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-01.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Ekstrakulikuler Futsal </h4>
+                    <ul>
+                      <li><span>Pembina:</span> Nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-02.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Pramuka </h4>
+                    <ul>
+                      <li><span>Pembima:</span> Nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-03.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Osis SMA Xaverius Baturaja</h4>
+                    <ul>
+                      <li><span>Pembina :</span> Nama Pembina</li>
+                      <li><span>Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-01.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Ekstrakulikuler Futsal</h4>
+                    <ul>
+                      <li><span>Pembina:</span> Nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-02.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Pramuka</h4>
+                    <ul>
+                      <li><span>Pembina:</span> Nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-03.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Osis SMA Xaverius Baturaja</h4>
+                    <ul>
+                      <li><span>Pembina:</span> nama Pembima</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-01.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Ekstrakulikuler Futsal</h4>
+                    <ul>
+                      <li><span>Pembina</span> nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-02.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Pramuka</h4>
+                    <ul>
+                      <li><span>Pembina:</span> nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="thumb">
+                <img src="assets/images/featured-03.jpg" alt="">
+                <div class="hover-effect">
+                  <div class="content">
+                    <h4>Osis SMA Xaverius Baturaja</h4>
+                    <ul>
+                      <li><span>Pembina:</span> nama Pembina</li>
+                      <li><span>Jumlah Anggota:</span> 50 orang</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="pricing-plans">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-heading text-center">
+            <h6>SMA Xaverius Baturaja</h6>
+            <h4>Website Ekstrakulikuler <em>Olahraga</em>, <em>Seni </em>& <em>Organisasi</em></h4>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing-item">
+            <img src="assets/images/pricing-01.jpg" alt="">
+            <h4>Ekstrakulikuler<br>Olahraga</br></h4>
+            <ul class="first-plan">
+              <li>Ekskul Futsal</li>
+              <li>Ekskul Basket</li>
+              <li>Ekskul Bulutangkis</li>
+            </ul>
+            <span class="price"></span>
+            <div class="border-button">
+              <a href="login.php" id="register_form">DAFTAR SEKARANG!!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing-item">
+            <img src="assets/images/pricing-02.jpg" alt="">
+            <h4>Ekstrakulikuler<br>Seni</br></h4>
+            <ul class="second-plan">
+              <li>Ekskul Tari</li>
+              <li>EKskul Melukis</li>
+              <li>Paduan Suara</li>
+            </ul>
+            <span class="price"></span>
+            <div class="border-button">
+              <a href="login.php">DAFTAR SEKARANG!!</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="pricing-item">
+            <img src="assets/images/pricing-03.jpg" alt="">
+            <h4>Organisasi<br></br></h4>
+            <ul class="third-plan">
+              <li>Osis SMA Xaverius Baturaja</li>
+              <li>Pramuka</li>
+              <li>PMR (Palang Merah Remaja)</li>
+            </ul>
+            <span class="price"></span>
+            <div class="border-button">
+              <a href="login.php">DAFTAR SEKARANG!!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <p>Copyright © 2022 <a href="#">SnapX</a> Photo Contest Co., Ltd. All rights reserved. 
+          
+          Design: <a title="CSS Templates" rel="sponsored" href="https://templatemo.com" target="_blank">TemplateMo</a></p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/wow.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+
+  </body>
+</html>
